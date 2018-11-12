0c42f5068e0058d86b42992d14c8c85207c704df
Miguel Rojas Cortes <migueldrojascortes@icloud.com>
2018-11-12 17:37:54 -0500

    Updating with real shit, stupid assholes

--- Uncloud/UnCloud-Starter/UnCloudNotes/AttachmentToImageAttachmentMigrationPolicyV3toV4.swift
+++ Uncloud/UnCloud-Starter/UnCloudNotes/AttachmentToImageAttachmentMigrationPolicyV3toV4.swift
@@ -1,56 +1,74 @@
-//
-//  AttachmentToImageAttachmentMigrationPolicyV3toV4.swift
-//  UnCloudNotes
-//
-//  Created by Miguel D Rojas Cortés on 11/12/18.
-//  Copyright © 2018 Ray Wenderlich. All rights reserved.
-//
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-import UIKit
+import Foundation
 import CoreData
+import UIKit
+
+let errorDomain = "Migration"
 
 class AttachmentToImageAttachmentMigrationPolicyV3toV4: NSEntityMigrationPolicy {
   
-  let errorDomain = "Migration"
-  
-  // Core Data takes instances of managed objects from the old data model, and creates corresponding instances that work with the new data model.
   override func createDestinationInstances(forSource sInstance: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
-    let newAttachment = ImageAttachment.init(entity: ImageAttachment.entity(), insertInto: manager.destinationContext)
     
-    try traversePropertyMappings(mapping: mapping, block: { propertyMapping, destinationName in
-      if let valueExpression = propertyMapping.valueExpression {
-        let context: NSMutableDictionary = ["source": sInstance]
-        guard let destinationValue = valueExpression.expressionValue(with: sInstance, context: context) else { return }
-        newAttachment.setValue(destinationValue, forKey: destinationName)
-        
-        if let image = sInstance.value(forKey: "image") as? UIImage {
-          newAttachment.setValue(image.size.width, forKey: "width")
-          newAttachment.setValue(image.size.height, forKey: "height")
+    let description = NSEntityDescription.entity(forEntityName: "ImageAttachment", in: manager.destinationContext)
+    let newAttachment = ImageAttachment(entity: description!, insertInto: manager.destinationContext)
+    
+    func traversePropertyMappings(block: (NSPropertyMapping, String) -> ()) throws {
+      if let attributeMappings = mapping.attributeMappings {
+        for propertyMapping in attributeMappings {
+          if let destinationName = propertyMapping.name {
+            block(propertyMapping, destinationName)
+          } else {
+            let message = "Attribute destination not configured properly"
+            let userInfo = [NSLocalizedFailureReasonErrorKey: message]
+            throw NSError(domain: errorDomain, code: 0, userInfo: userInfo)
+          }
         }
-        
-        let body = sInstance.value(forKeyPath: "note.body") as? NSString ?? ""
-        newAttachment.setValue(body.substring(to: 80), forKey: "caption")
-        manager.associate(sourceInstance: sInstance, withDestinationInstance: newAttachment, for: mapping)
+      } else {
+        let message = "No Attribute Mappings found!"
+        let userInfo = [NSLocalizedFailureReasonErrorKey: message]
+        throw NSError(domain: errorDomain, code: 0, userInfo: userInfo)
       }
-    })
-  }
-  
-  func traversePropertyMappings(mapping:NSEntityMapping, block: (NSPropertyMapping, String) -> ()) throws {
-    if let attributeMappings = mapping.attributeMappings {
-      for propertyMapping in attributeMappings {
-        if let destinationName = propertyMapping.name {
-          block(propertyMapping, destinationName)
-        } else {
-          let message = "Attribute destination not configured properly"
-          let userInfo = [NSLocalizedFailureReasonErrorKey: message]
-          throw NSError(domain: errorDomain, code: 0, userInfo: userInfo)
-        }
-      }
-    } else {
-      let message = "No Attribute Mappings found!"
-      let userInfo = [NSLocalizedFailureReasonErrorKey: message]
-      throw NSError(domain: errorDomain, code: 0, userInfo: userInfo)
     }
+    
+    try traversePropertyMappings { propertyMapping, destinationName in
+      guard let valueExpression = propertyMapping.valueExpression else { return }
+      
+      let context: NSMutableDictionary = ["source": sInstance]
+      guard let destinationValue = valueExpression.expressionValue(with: sInstance, context: context) else { return }
+      
+      newAttachment.setValue(destinationValue, forKey: destinationName)
+    }
+    
+    if let image = sInstance.value(forKey: "image") as? UIImage {
+      newAttachment.setValue(image.size.width, forKey: "width")
+      newAttachment.setValue(image.size.height, forKey: "height")
+    }
+    
+    let body = sInstance.value(forKeyPath: "note.body") as? NSString ?? ""
+    let caption = body.length > 80 ? body.substring(to: 80) : body as String
+    newAttachment.setValue(caption, forKey: "caption")
+    
+    manager.associate(sourceInstance: sInstance, withDestinationInstance: newAttachment, for: mapping)
   }
-  
 }
